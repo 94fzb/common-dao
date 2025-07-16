deb1ca2472910ee5af856ddecde66341f1a132ba
xiaochun <90xchun@gmail.com>
2025-07-16 23:01:06 +0800

    Fix

--- src/main/java/com/hibegin/common/dao/DataSourceWrapperImpl.java
+++ src/main/java/com/hibegin/common/dao/DataSourceWrapperImpl.java
@@ -58,7 +58,7 @@ public class DataSourceWrapperImpl extends HikariDataSource implements DataSourc
     public String getDbInfo() {
         try {
             if (isWebApi()) {
-                return "webapi/1.0.10";
+                return "webapi/1.0.14";
             }
             return (String) new DAO(this).queryFirstObj("select version()");
         } catch (Exception e) {
