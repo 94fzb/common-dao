35d70b934efacb83c88be0b112c8cb2c17f24839
xiaochun <90xchun@gmail.com>
2025-07-28 15:12:01 +0800

    [shell-release]release version 1.0.17

---
+++ src/main/java/com/hibegin/common/dao/FlexibleBooleanAdapter.java
@@ -0,0 +1,31 @@
+package com.hibegin.common.dao;
+
+import com.google.gson.JsonParseException;
+import com.google.gson.TypeAdapter;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
+
+public class FlexibleBooleanAdapter extends TypeAdapter<Boolean> {
+    @Override
+    public void write(JsonWriter out, Boolean value) throws java.io.IOException {
+        out.value(value);
+    }
+
+    @Override
+    public Boolean read(JsonReader in) throws java.io.IOException {
+        switch (in.peek()) {
+            case BOOLEAN:
+                return in.nextBoolean();
+            case NUMBER:
+                return in.nextDouble() != 0;
+            case STRING:
+                String str = in.nextString();
+                return ResultValueConvertUtils.toBoolean(str);
+            case NULL:
+                in.nextNull();
+                return false;
+            default:
+                throw new JsonParseException("Cannot parse to boolean");
+        }
+    }
+}
